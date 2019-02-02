63b2afe3b26817834cb9b394cfcdceea0d3e479c
Julia Wang <julia135793@gmail.com>
2019-02-03 00:05:33 +0800

    Updates

---
+++ precache-manifest.f3c480e5a946706224394581f9e0ce1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e443de7ecbae72c98094",
+    "url": "/julia30040.github.io/static/css/main.7c6ceebe.chunk.css"
+  },
+  {
+    "revision": "e443de7ecbae72c98094",
+    "url": "/julia30040.github.io/static/js/main.e443de7e.chunk.js"
+  },
+  {
+    "revision": "da1c356ead4b057314cc",
+    "url": "/julia30040.github.io/static/js/1.da1c356e.chunk.js"
+  },
+  {
+    "revision": "3c4a7c1b7c0b5a5cc0ca",
+    "url": "/julia30040.github.io/static/js/runtime~main.3c4a7c1b.js"
+  },
+  {
+    "revision": "0f99d2326b9d0515063637ed1895de0a",
+    "url": "/julia30040.github.io/static/media/avatar.0f99d232.png"
+  },
+  {
+    "revision": "0d8bf6bae67556387c6af3a58658d435",
+    "url": "/julia30040.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d83e39c6e7d300ca81a77f196f3bf865.js"
+  "/julia30040.github.io/precache-manifest.f3c480e5a946706224394581f9e0ce1e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/julia30040.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
